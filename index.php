fcab34a6ad493bc787772cc84284bf8ebf468ed2
Braulio <brawlyo@hotmail.com>
2019-06-27 00:23:11 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<title>Pokemon API</title>
+</head>
+<body onload="show();">
+
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/modal.css">
+<link rel="stylesheet" type="text/css" href="css/ui.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<h1>Pokemon Generator</h1>
+<!-- <h2 id="loader">Carregando...</h2> -->
+
+<!-- <input type="submit" value="o_nome_do_Botão" onClick="calcular()"> -->
+<!-- <div class="nav-btn-container">
+	<button class="prev-btn">Prev</button>
+	<button class="next-btn">Next</button>
+</div> -->
+
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-xl-8 col-xs-6">
+			<div class="form-group">
+				<input style="width: 60%;float: right;" type="text" id="tags" class="form-control" name="pokemonNome" placeholder="Ex: pikachu">
+			</div>
+		</div>
+		<div class="col-xl-4 col-xs-6" style="padding-left: 0">
+			&nbsp<button style="float: left;" type="button" class="btn btn-info" name="pesquisarName" id="pesquisarNome">Pesquisar</button>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-xl-12 col-xs-12">
+			<ul class="ul-custom">
+			</ul>
+		</div>
+	</div>
+
+	<div class="row" style="">
+		
+			<button style="width: 30%; text-align: center; margin: 0 auto; height: 60px; font-size: 26px; font-weight: bold; margin-bottom: 50px" type="button" class="next-btn btn btn-success">Ver Mais</button>
+		</div>
+	
+</div>
+
+<!-- Modal Struture -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,260 @@
+// $(document).ready(function() {
+//   $("#loader").hide();
+//   $("#button").click(function() {
+//     $(".container").hide();
+//     $("#loader").fadeIn();
+//     $.ajax({
+//       type: "GET",
+//       url: "https://pokeapi.co/api/v2/pokemon/" + randomNumber(),
+//       data: {limit: 5, order: 'desc'},
+//       success: function(data) {
+//         console.log(data);
+//         $("#name").html(data.name.toUpperCase());
+//         $("#pokemon_id").html(data.id);
+//         $("#experience").html(data.base_experience);
+//         $("#img").attr("src", data.sprites.front_default);
+//         $("#loader").hide();
+//         $(".container").fadeIn();
+//       }
+//     })
+//   }) 
+// });
+
+var page = 0,
+  offset = 10,
+  totalrecord = 0;
+
+  $(".prev-btn").on("click", function(){
+    if (page > 0) {
+      page--;
+      show();
+    }
+    console.log("Prev Page: " + page);
+  });
+
+  $(".next-btn").on("click", function(){
+    if (page * offset < totalrecord) {
+      page += 10;
+      show();
+    }
+    console.log("Next Page: " + page);
+  });
+
+function randomNumber() {  
+    return Math.floor((Math.random() * 500) + 1);  // I have no idea how many Pokemon exist...
+}
+
+function show() {
+  // $('ul li').remove();
+
+ 
+  $.ajax({
+      type: "GET",
+      dataType: "json",
+      // url: "https://pokeapi.co/api/v2/pokemon/" + randomNumber(),
+      url: "https://pokeapi.co/api/v2/pokemon/",
+      data: {offset: page, limit: offset},
+      success: function(data) {
+        console.log(data);
+
+        totalrecord = 3000;
+        // console.log(data['results'][0]);
+        $(data['results']).each(function(index, item){
+
+
+          // console.log(data['results'][0].name);
+
+          var name = capitalizeFirstLetter(data['results'][index]['name']);
+          buscar(data['results'][index]['name']);
+
+
+
+          // console("AAAAA = " + imageUrl);
+          // console.log("index= "+index);
+          // $('ul').append('<li class="li-custom" id="'+index+'"><img id="img" class="toggleHideShow" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ (index+1) +'.png"></img> <h3><br><span id="name">'+ name +'</span></h3><hr><h4>Ver Detalhes</h4></li>');
+        });
+      },
+    }) //ajax end.
+} // end function show()
+
+
+
+
+function beforeBuscar() {
+  var nameInclude = "";
+  $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: "https://pokeapi.co/api/v2/pokemon/",
+       data: {limit: 365, order: 'desc'},
+      success: function(data) {
+        $(data['results']).each(function(index, item){
+          var name = data['results'][index]['name'];
+          // console.log(name);
+          nameInclude += name;
+          nameInclude += " ";
+        });
+        console.log(nameInclude);
+      },
+    }); //ajax end
+    setTimeout(function(){
+      var array = nameInclude.split(" ");
+      console.log(array);
+      var resultado = [""];
+      $( function() {
+      var availableTags = array;
+      $( "#tags" ).autocomplete({
+        source: availableTags
+      });
+      } );
+    }, 1000);
+}
+
+
+
+function buscar(nomepokemon) {
+
+
+
+  
+  
+  $.ajax({
+      type: "GET",
+      dataType: "json",
+      // url: "https://pokeapi.co/api/v2/pokemon/" + randomNumber(),
+      url: "https://pokeapi.co/api/v2/pokemon/"+ nomepokemon,
+      // data: {limit: 5, order: 'desc'},
+      success: function(data) {
+        
+
+        // console.log(data['results'][0]);
+        // $(data['results']).each(function(index, item){
+        //   console.log(data['results'][0].name);
+        //   $('ul').append('<li><img id="img" class="toggleHideShow" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ (index+1) +'.png"></img> <h3>Name: <br><span id="name">'+ data['results'][index].name +'</span></h3></li>');
+         var imageUrl = data['sprites']['front_default'];
+         var name = capitalizeFirstLetter(data['forms'][0]['name']);
+         var id = (data['id'] - 1);
+
+
+
+
+        // });
+        $(data).each(function(index, item){
+          // console.log(data['results']);
+          // $('ul li').remove();
+          $('ul').append('<li class="li-custom" id="'+id+'"><img id="img" class="toggleHideShow" src="'+ imageUrl +'"></img> <h3>Name: <br><span id="name">'+ name  +'</span></h3><hr><h4>Ver Detalhes</h4></li>');
+
+
+          
+
+      
+        });
+        
+        $('button[name=pesquisarName').html('Pesquisar');  
+
+      },
+      error: function(){
+       alert('request failed');
+       $('button[name=pesquisarName').html('Pesquisar');
+      },
+      beforeSend: function() {
+          // setting a timeout
+          $('button[name=pesquisarName').html('Processando...');
+
+      },
+    }); //ajax end
+
+    setTimeout(function(){
+     $("li").on("click", function(){
+      var index = $(this).attr('id');
+      details(index);
+     });
+    }, 1000);
+
+}
+
+// buscar('pikachu');
+  
+function details(index) {
+  // $('ul li').remove();
+  var index = (parseInt(index) + 1);
+
+  $.ajax({
+      type: "GET",
+      dataType: "json",
+      // url: "https://pokeapi.co/api/v2/pokemon/" + randomNumber(),
+      url: "https://pokeapi.co/api/v2/pokemon/" + index,
+      // data: {limit: no, order: 'desc'},
+      success: function(data) {
+        var imageUrl = data['sprites']['front_default'];
+        var name = capitalizeFirstLetter(data['forms'][0]['name']);
+        var weight = data['weight'];
+        var types = data['types'];
+        var experience = data['base_experience'];
+        var typesConcat = [];
+
+        // console.log(types);
+      for (var i = 0; i < types.length; i++) {
+        typesConcat[i] = capitalizeFirstLetter(types[i]['type']['name']);
+      }
+
+      var typesConcat = typesConcat.join('/');
+
+        $("#exampleModalCenter").modal();
+        $('.modal-body').html('<div class="infoWrap"> <div class="monsterData"> <span>ID: </span> <h2>0'+index+'</h2> <div class="circleWrap"> <div class="frontCircle"></div><div class="backCircle"></div></div></div><div class="monsterInfo"> <img src="'+imageUrl+'" alt="pokeMon" class="pokemon"/> <div class="pokemonLevel"> <h2 class="greenWord pokemonName">'+name+'</h2> <div class="levelBar"></div><div class="HP"> <span>100</span>/<span>100</span>HP </div></div><div class="pokemonInfo"> <div class="infoBox"> <h3 class="greenWord">'+weight+' Kg</h3> <span class="grayWord">PESO</span> </div><div class="infoBox-50"> <h3 class="greenWord">'+typesConcat+'</h3><span class="grayWord">TIPO</span> </div><div class="infoBox"> <h3 class="greenWord">'+experience+'</h3> <span class="grayWord">EXP</span> </div></div><div class="roundBox"> <button class="upgradeBtn">POWER UP</button> </div><div class="roundBox"> <button class="upgradeBtn">EVOLVE</button> </div> </div></div>');
+        console.log(data);
+
+      }
+    }) //ajax end.
+} // end function show()
+
+
+  
+
+
+
+  $(document).ready(function(){
+    $('#pesquisarNome').click(function() {
+      var pokemonNome = $('input[name=pokemonNome').val().toLowerCase();
+      if (pokemonNome != '') {
+        $('ul li').remove();
+        buscar(pokemonNome);
+      } else {
+        show();
+      } 
+      console.log('Botão id="pesquisarId" clicado e função disparada');
+    });
+
+    // Dispara AutoComplete
+    $("input[name='pokemonNome']").click(function(){
+      beforeBuscar();
+    });
+
+  });
+
+
+
+    function capitalizeFirstLetter(string) {
+    return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+
+    setTimeout(function(){
+     $("li").on("click", function(){
+      var index = $(this).attr('id');
+      details(index);
+     });
+    }, 1000);
+
+
+
+   
+
+
+
+
+
+
+
+
+  
+
